a5c4ab4fce1fa0deaa6850bb94c69f10b9556efa
jschnoerr <Johannes.Schnoerr@adesso.de>
2024-05-17 13:36:49 +0200

    update job component

--- ng-job-search-main/ng-job-search-main/src/app/job/job.component.ts
+++ ng-job-search-main/ng-job-search-main/src/app/job/job.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JobService } from './job.service';
+import { DetailedJob, Job } from './job';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-job',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './job.component.html',
   styleUrl: './job.component.css'
 })
-export class JobComponent {
+export class JobComponent implements OnInit {
 
+  jobList: Job[] = [];
+  detailedJoblist: DetailedJob[] = [];
+
+  constructor(private jobService: JobService) { }
+
+  ngOnInit(): void {
+    this.jobService.getJob().subscribe(
+      (data: Job[]) => {
+        this.jobList = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    this.jobService.getDetailedJob().subscribe(
+      (data: DetailedJob[]) => {
+        this.detailedJoblist = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+
+  }
 }
