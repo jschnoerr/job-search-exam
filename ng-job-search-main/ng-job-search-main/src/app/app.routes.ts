430c85aa288170254ca94198150f44f0fe42d6e4
jschnoerr <Johannes.Schnoerr@adesso.de>
2024-05-17 13:36:13 +0200

    update Routing functionality

--- ng-job-search-main/ng-job-search-main/src/app/app.routes.ts
+++ ng-job-search-main/ng-job-search-main/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { JobComponent } from './job/job.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'joblist', pathMatch: 'full' },
     {path: 'joblist', component: JobComponent}
 ];
