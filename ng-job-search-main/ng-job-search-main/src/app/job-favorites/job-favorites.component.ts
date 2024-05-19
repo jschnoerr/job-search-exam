bc8fdbd521894ef5372481a3b0ab1b382e9f72cb
jschnoerr <Johannes.Schnoerr@adesso.de>
2024-05-19 21:38:06 +0200

    setup basic job-favorites component

--- ng-job-search-main/ng-job-search-main/src/app/job-favorites/job-favorites.component.ts
+++ ng-job-search-main/ng-job-search-main/src/app/job-favorites/job-favorites.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { JobService } from '../job/job.service';
+import { Job, DetailedJob } from '../job/job';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-job-favorites',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './job-favorites.component.html',
   styleUrl: './job-favorites.component.css'
 })
 export class JobFavoritesComponent {
+
+  jobList: Job[] = [];
+  detailedJoblist: DetailedJob[] = [];
+  favoriteJobIDs: number[] = [];
+
+  constructor(private jobService: JobService) {
+    this.jobService.getJob().subscribe(
+      (data: Job[]) => {
+        this.jobList = data;
+        this.jobList.map((job) => {
+        })
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    this.jobService.getDetailedJob().subscribe(
+      (data: DetailedJob[]) => {
+        this.detailedJoblist = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+    this.jobService.favoriteJobs$.subscribe(
+      (favoriteJobIDs: number[]) => {
+        this.favoriteJobIDs = favoriteJobIDs;
+      }
+    );
+   }
 
 }
