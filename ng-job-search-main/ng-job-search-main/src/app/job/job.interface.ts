71eb0a33b1977650b75f26ad4bc7bdd8dba713de
jschnoerr <Johannes.Schnoerr@adesso.de>
2024-05-21 12:00:37 +0200

    make all properties of job.interface readonly

--- ng-job-search-main/ng-job-search-main/src/app/job/job.interface.ts
+++ ng-job-search-main/ng-job-search-main/src/app/job/job.interface.ts
@@ -1,15 +1,15 @@
 export interface Job {
-    id: number;
-    companyName: string;
-    title: string;
-    companyLogo: string;
-    reference: string;
+    readonly id: number;
+    readonly companyName: string;
+    readonly title: string;
+    readonly companyLogo: string;
+    readonly reference: string;
 }
 
 export interface DetailedJob extends Job {
-    location: string;
-    industries: string[];
-    types: string[];
-    description: string;
-    publishDate: string;
+    readonly location: string;
+    readonly industries: string[];
+    readonly types: string[];
+    readonly description: string;
+    readonly publishDate: string;
   }
